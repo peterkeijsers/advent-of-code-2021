9cf6cf2fca150d0203626138f8343db8a11c72a8
Peter Keijsers <peter.keijsers@brightalley.nl>
2021-12-01 23:03:52 +0100

    Added solution for part 2 of day 1

--- day1/index.php
+++ day1/index.php
@@ -2,6 +2,7 @@
 
 $data = explode(PHP_EOL, file_get_contents('./data.txt'));
 
+// Part 1
 $lastValue = null;
 $valuesIncreased = array_filter($data, function($value) use (&$lastValue){
     $valueHasIncreased = ($lastValue && $value > $lastValue);
@@ -9,4 +10,23 @@ $valuesIncreased = array_filter($data, function($value) use (&$lastValue){
     return $valueHasIncreased;
 });
 
+echo nl2br("Number of increased values \n");
 echo count($valuesIncreased);
+
+// Part 2
+$previousWindow = [];
+$currentWindow = [];
+$increasedWindows = 0;
+foreach($data as $key => $measurement) {
+    $currentWindow = array_slice($data, $key, 3, true);
+    if(count($currentWindow) != 3) break;
+
+    if($previousWindow && $currentWindow && array_sum($currentWindow) > array_sum($previousWindow)){
+        $increasedWindows++;
+    }
+
+    $previousWindow = $currentWindow;
+};
+
+echo nl2br("\n Number of increased chunks of 3 data-points \n");
+echo $increasedWindows;
